44fafa24d8e27b3cdc60d8f5779e29f78468b125
rudolf <rudolf.mard@gmail.com>
2023-02-10 15:28:02 +0200

    Tehtävä 3.10: puhelinluettelon backend step10

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ app.get("/info", (req, res) => {
     res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${Date(Date.now()).toString()}</p>`)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
